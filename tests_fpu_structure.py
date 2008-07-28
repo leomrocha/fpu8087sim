3ee78ae8fe42b725ab9572304894cdd52a3dc03c
leo.m.rocha <leo.m.rocha@fe499417-0353-0410-9793-07564672872e>
2008-07-28 15:02:19 +0000

    Agregados algunos casos de prueba y la documentación respectiva.

---
+++ tests_fpu_structure.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Casos de prueba de isntruction_set.py
+"""
+import unittest
+from instruction_set import *
+
+#Test LDS
+
+class TestPila(unittest.TestCase):
+	pass
